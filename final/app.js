dbc9c934721ced61a0972593adac075ce1a00723
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-01-26 14:55:53 +0700

    Update

--- final/app.js
+++ final/app.js
@@ -1,11 +1,13 @@
-const title = <h1>My first React App</h1>;
+const desc = 'I just learned how to create a React node and render it into the DOM.';
+const titleId = 'main-title';
+const name = 'Frank';
 
-const desc = <p>I just learned how to create a React node and render it into the DOM.</p>;
-
-const header = React.createElement(
-  'header',
-  null,
-  title, desc
+const header = (
+  <header>
+    {/* This is a comment */}
+    <h1 id={titleId}>{name}'s first React Element!</h1>
+    <p className="main-desc">{desc}</p>
+  </header>
 );
 
 ReactDOM.render(header, document.getElementById('root'));

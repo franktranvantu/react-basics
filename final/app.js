74b42eb39773b697ff1f84020e041a39e3732bc4
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-01-26 14:06:31 +0700

    Update

--- final/app.js
+++ final/app.js
@@ -4,4 +4,16 @@ const title = React.createElement(
   'My first React App'
 );
 
-console.log(title)
+const desc = React.createElement(
+  'p',
+  null,
+  'I just learned how to create a React node and render it into the DOM.'
+);
+
+const header = React.createElement(
+  'header',
+  null,
+  title, desc
+);
+
+ReactDOM.render(header, document.getElementById('root'));

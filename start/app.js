238d280e5c793048306faa039fb294fa2c331bed
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-01-26 15:32:07 +0700

    Update

--- start/app.js
+++ start/app.js
@@ -7,4 +7,20 @@ const Header = () => {
   );
 }
 
-ReactDOM.render(<Header />, document.getElementById('root'));
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">
+        Frank
+      </span>
+
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">35</span>
+        <button className="counter-action increment"> + </button>
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<Player />, document.getElementById('root'));

cbeb860c4946a3fc43cba05bad282fdcf3c11745
Frank Tran <franktranvantu@gmail.com>
2021-02-02 22:56:41 +0700

    Update

---
+++ README.md
@@ -0,0 +1,3 @@
+You'll often see components defined as arrow functions. 
+
+Even though there's no real benefit to writing a component as an arrow function, it provides a more concise syntax.
--- final/app.js
+++ final/app.js
@@ -1,16 +1,17 @@
-const Header = () =>
-  <header>
-    <h1>Scoreboard</h1>
-    <span className="stats">Players: 1</span>
-  </header>
+const Header = () => {
+  return (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 1</span>
+    </header>
+  )
+}
 
-// const Header = () => {
-//   return (
-//     <header>
-//       <h1>Scoreboard</h1>
-//       <span className="stats">Players: 1</span>
-//     </header>
-//   )
-// }
+// const Header = () =>
+//   <header>
+//     <h1>Scoreboard</h1>
+//     <span className="stats">Players: 1</span>
+//   </header>
+
 
 ReactDOM.render(<Header />, document.getElementById('root'));
